b906360be03736fedaad905e8e9faf70b8e5e8c1
StevenSerrien <steven@xpenditure.com>
2019-08-17 20:19:09 +0200

    quick draft for pricing

---
+++ src/_modules/organisms/info-block/tests/info-block.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import InfoBlock from '../info-block';
+
+describe('InfoBlock View', function() {
+
+  beforeEach(() => {
+    this.infoBlock = new InfoBlock();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.infoBlock);
+  });
+
+});
