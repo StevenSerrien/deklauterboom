f7499c434525f428b650f455c7b5375728a5d8d3
StevenSerrien <steven@xpenditure.com>
2019-08-18 16:50:48 +0200

    WIP - Commit before starting on foundation form validation

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -5,10 +5,21 @@
 
 import $ from "jquery";
 import Link from "../_modules/link/link";
+import Toastify from 'toastify-js';
 
 $(() => {
   new Link(); // Activate Link modules logic
   // console.log('Welcome to Yeogurt!');
+
+  var toastConfig = {
+    text: "This is a toast",
+    duration: 3000,
+    newWindow: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: 'left', // `left`, `center` or `right`
+    className: "toast",
+    onClick: function(){} // Callback after click
+  }
 
   var contactFormActivated = false;
 
@@ -26,6 +37,12 @@ $(() => {
     contactFormActivated = true;
     form = $(form)[0];
 
+    var successTitle = $(form).data('success-title');
+
+    console.log('>>>', successTitle);
+    var successText = $(form).data('success-text');
+
+
     var firstName = $("#first_name").val();
     var lastName = $("#last_name").val();
     var email = $("#email").val();
@@ -33,31 +50,42 @@ $(() => {
     var telephone = $("#telephone").val();
 
 
-    $.ajax({
-      type: "POST",
-      url: actionUrl + "send_email",
-      data:
-        "first_name=" +
-        firstName +
-        "&last_name=" +
-        lastName +
-        "&email=" +
-        email +
-        "&telephone=" +
-        telephone +
-        "&message=" +
-        message,
-      success: function(text) {
-        contactFormActivated = false;
-        if (text && text.message == "success") {
-          form.reset();
-        } else if (text && text.message == "failure_email") {
-          alert("Er ging iets mis. Bericht niet verstuurd.");
-        }
-      },
-      error: function() {
-        alert("Er ging iets mis. Bericht niet verstuurd.");
-      }
-    });
+    showToast('<span class="title">' + successTitle + '</span> <br/ > <span class="text">' + successText + '</span>', 'success');
+    // $.ajax({
+    //   type: "POST",
+    //   url: actionUrl + "send_email",
+    //   data:
+    //     "first_name=" +
+    //     firstName +
+    //     "&last_name=" +
+    //     lastName +
+    //     "&email=" +
+    //     email +
+    //     "&telephone=" +
+    //     telephone +
+    //     "&message=" +
+    //     message,
+    //   success: function(text) {
+    //     contactFormActivated = false;
+    //     if (text && text.message == "success") {
+    //       form.reset();
+    //       showToast(text, success);
+    //     } else if (text && text.message == "failure_email") {
+    //       alert("Er ging iets mis. Bericht niet verstuurd.");
+    //     }
+    //   },
+    //   error: function() {
+    //     alert("Er ging iets mis. Bericht niet verstuurd.");
+    //   }
+    // });
+  }
+
+
+  function showToast(text, type) {
+    var config = toastConfig;
+    config.text = text;
+    config.className = type;
+
+    Toastify(config).showToast();
   }
 });
